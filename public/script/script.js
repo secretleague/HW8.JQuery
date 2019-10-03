f53d9bc639e5d35505d2276bb4497ca27e674e2f
H1op3ts <plywoodstore@inbox.ru>
2019-10-04 00:06:06 +0300

    Доработал script.js

--- public/script/script.js
+++ public/script/script.js
@@ -1,78 +1,91 @@
 
 $(function() {
+    getImages()
+    getVisibleImage()
+    getVisibleImageIndex()
 
+    let $slider = $('.slider-wrapper');
+    let $navButtons = $('.slider-button');
+    let $imagesQuantity = $images.length;
+    let $dots = $('.slider__dot');
+    
     function setVisibleImage(i) {
-        $images.each((index, el) => {
-            $(el).removeClass('slider__img--visible');
-            });
-        $images[i].classList.add('slider__img--visible');
+
+        getVisibleImage()
+
+        $visibleImage.removeClass('slider__img--visible');
+        $($images[i]).addClass('slider__img--visible');
+
     }
 
     function setActiveDot(i) {
-        $dots.each((index, el) => {
-            $(el).removeClass('slider__dot--active');
-            });
-        $dots[i].classList.add('slider__dot--active');
+
+        getactiveDot()
+
+        $($activeDot).removeClass('slider__dot--active');
+        $($dots[i]).addClass('slider__dot--active');
+
     }
 
     function getImages() {
-        return $images = $( '.slider__img '); 
+        return $images = $('.slider__img'); 
     }
 
     function getVisibleImageIndex() {
-        return $visibleImageIndex = $('li').index($visibleImage);
+        return $visibleImageIndex = $images.index($visibleImage);
     }
 
     function getVisibleImage() {
         return $visibleImage = $('.slider__img--visible');
     }
 
-
-    let $slider = $('.slider-wrapper');
-    let $navButtons = $('.slider-button');
-    let $imagesQuantity = getImages().length;
-    let $dots = $('.slider__dot');
-    
-
-    getVisibleImage()
-    getVisibleImageIndex()
-    
+    function getactiveDot() {
+        return $activeDot = $('.slider__dot--active');
+    }
+  
     $dots.mouseenter(function(e) {
+
         $(this).addClass('slider__dot--hover');
         }).mouseout(function(e) {
         $(this).removeClass('slider__dot--hover');
+
     });
 
     $dots.click(function(e) {
+
         let $index = $(this).index();
         setVisibleImage($index);
         setActiveDot($index);
         $visibleImageIndex = $index;  
+
     })
 
     $slider.mouseenter(function(e) {
+
         $navButtons.addClass('slider-button--visible');
         }).mouseleave(function(e) {
         $navButtons.removeClass('slider-button--visible');
+
         }); 
     
-    
     $navButtons.mouseover(function(e) {
+
         $(this).addClass('slider-button--hover');
         }).mouseout(function(e) {
         $(this).removeClass('slider-button--hover');
+
     });
 
     $navButtons.click(function(e) {
 
-        $(this).hasClass('slider__previous-button') ? $visibleImageIndex-- : $visibleImageIndex++ ;
+        $(this).hasClass('slider__previous-button') ? $visibleImageIndex--: $visibleImageIndex++ ;
         if ($visibleImageIndex < 0) {
             $visibleImageIndex = $imagesQuantity - 1;
         }
-        else if ($visibleImageIndex > 3) {
+        else if ($visibleImageIndex > ($imagesQuantity - 1)) {
             $visibleImageIndex = 0;
         }
-               
+        
         setVisibleImage($visibleImageIndex);
         setActiveDot($visibleImageIndex);
     })
